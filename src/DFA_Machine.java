56d98f500bd8b1472c72a84cc1eccf7b40f4eae1
tahere fahimi <t.fahimi1998@gmail.com>
2020-04-13 16:22:11 +0430

    initial the structure

---
+++ src/DFA_Machine.java
@@ -0,0 +1,18 @@
+// DFA machine
+
+import java.util.ArrayList;
+
+public class DFA_Machine {
+    private ArrayList alphabet = new ArrayList<String>();
+    private ArrayList states = new ArrayList<State>();
+    private ArrayList final_states = new ArrayList<State>();
+    private State start_state ;
+
+    //read the file and generate the states and edges
+    public DFA_Machine() {
+
+    }
+
+
+
+}
